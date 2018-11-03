4cef0dd7e02068af552caa2bb3015a0954d18551
sheylasantos <44684222+sheylasantos@users.noreply.github.com>
2018-11-03 03:11:24 -0300

    Update listar_t.c

--- listar_t.c
+++ listar_t.c
@@ -4,6 +4,22 @@
 
 void listar_t(){
 	FILE *bd;
-	bd = fopen("BD ITP.txt", "r");
-	for(int i = 0; i < )
+	char *nome_t = malloc(sizeof(char)*100);
+	int ntabelas;
+
+	bd = fopen("BD-ITP.txt", "r");
+	if (bd == NULL){
+		printf("Erro na abertura do arquivo!");
+		exit(1);
+	}else{
+		rewind(bd);
+		fscanf(bd, "|ntabelas = %d|", &ntabelas);
+		printf("-----TABELAS do banco BD-----\n");
+		for(int i=0; i < ntabelas; i++){
+			fscanf(bd, "%s", nome_t);
+			printf("%s\n", nome_t);
+		}
+		free(nome_t);
+	}
+	fclose(bd);
 }
