56a01fd8aa906b09ab8241928a5da5e0837ca449
Hilton Thallyson <hilton.thallyson@hotmail.com>
2018-11-05 15:38:40 -0300

    Update listar_t.c

--- listar_t.c
+++ listar_t.c
@@ -5,22 +5,24 @@
 
 void listar_t(){
 	FILE *bd;
-	char *nome_t = malloc(sizeof(char)*100);
-	int ntabelas;
+	char *nome_t = malloc(sizeof(char)*100);//aloca espaço para uma string de tamanho 100.
+	int ntabelas;//variável para armazenar o n de tabelas.
 
-	bd = fopen("BD-ITP.txt", "r");
+	bd = fopen("BD-ITP.txt", "r");//abre o arquivo BD-ITP.txt em modo leitura.
+	//checa se o arqivo foi aberto corretamente:
 	if (bd == NULL){
 		printf("Erro na abertura do arquivo!");
 		exit(1);
 	}else{
-		rewind(bd);
-		fscanf(bd, "|ntabelas = %d|", &ntabelas);
+		rewind(bd);//coloca o cursor na posição inicial do arquivo
+		fscanf(bd, "|ntabelas = %d|", &ntabelas);//lê o valor de ntabelas do arquivo bd.
 		printf("-----TABELAS do banco BD-----\n");
+		//faz um loop para ler o nome das tabelas linha por linha e mostrar na tela.
 		for(int i=0; i < ntabelas; i++){
 			fscanf(bd, "%s", nome_t);
 			printf("%s\n", nome_t);
 		}
-		free(nome_t);
+		free(nome_t);//desaloca nome_t.
 	}
-	fclose(bd);
+	fclose(bd);//fecha o arquivo em bd.
 }
