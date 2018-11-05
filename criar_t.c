2073bca6216cd2aece9cf80c602c15f7cd7bef9b
sheylasantos <44684222+sheylasantos@users.noreply.github.com>
2018-11-05 02:21:56 -0200

    Update criar_t.c

--- criar_t.c
+++ criar_t.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include "funcoes.h"
 
-void criar_t(){
+int valida_chave(char chave[6]){
+	int i,n=0;		
+	for(i=0;i<strlen(chave);i++){
+		if(isdigit(chave[i])!=0){
+			n++;
+		}
+	}
+	if(n==strlen(chave)){
+		return 1;
+	}else{
+		return 0;
+	}	
+}
+
+void criar_t(FILE *bd){
 	FILE *arquivo;
 	FILE *bd;
+	char nome_arq[55];
 	char nome_t[50];
 	char nome_c[50];
 	int tipo_c = 0;
-	int chave;
+	//int chave;
+	char chave[6];
 	char opc;
 	int valor_i = 0;
 	float valor_f = 0;
@@ -19,7 +37,10 @@ void criar_t(){
 	printf("Digite o nome da tabela: ");
 	scanf("%s",nome_t);
 	printf("\n");
-	arquivo = fopen(nome_t,"a");
+	strcpy(nome_arq,nome_t);
+	strcat(nome_arq,".txt");
+	arquivo = fopen(nome_arq,"a");
+	//arquivo = fopen(nome_t,"a");
 
 	if(arquivo == NULL){
 		printf("Erro na criacao/abertura do arquivo!\n");
@@ -28,8 +49,15 @@ void criar_t(){
 		fprintf(arquivo, "|Tabela - %s|\n", nome_t);
 		printf("Criando a primeira coluna: ID\n");
 		printf("Digite o valor da primeira coluna: ");
-		scanf("%d",&chave);
-		fprintf(arquivo, "|ID = %d|",chave);
+		//scanf("%d",&chave);
+		scanf("%s",chave);
+		if(valida_chave(chave)==0){
+			do{
+				printf("Digite o valor INTEIRO da primeira coluna: ");
+				scanf("%s",&chave);
+			}while(valida_chave(chave)==0);
+		}
+		fprintf(arquivo, "|ID = %s|",chave);
 
 		printf("Deseja acrescentar uma nova coluna: (s/n)\n");
 		scanf(" %c", &opc);
@@ -63,9 +91,7 @@ void criar_t(){
 			printf("Deseja acrescentar uma nova coluna: (s/n)\n");
 			scanf(" %c", &opc);
 		}
-		bd = fopen("BD ITP.txt","a");
 		fprintf(bd, "|%s|\n", nome_t);
 	}
 	fclose(arquivo);
-	fclose(bd);
 }
