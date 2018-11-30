ee638f39e73c9027c8f117a7b9b077fcc80269c8
sheylasantos <44684222+sheylasantos@users.noreply.github.com>
2018-11-29 21:52:02 -0300

    Update apagar_t.c

--- apagar_t.c
+++ apagar_t.c
@@ -5,8 +5,6 @@
 #include "funcoes.h"
 #include "structs.h"
 
-int valida_chave(char *chave);
-int valida_float(char *valor);
 int valida_tab(char *nome_tab);
 void listar_t();
 
@@ -15,16 +13,10 @@ void apagar_t(){
 	FILE *newbd;//arquivo que recebe a nova versao do original;
 	variaveis *tipo_var = malloc(sizeof(variaveis));
 	int ntabelas;
-	//int i = 0, j=0, k=0, t=0, g=0, h=0, u=0;// contadores;
-	int r = 0;// variavel que recebe o resultado dos testes de tipo;
 	int tem_tab = 0; //variavel para verificar se a chave inserida ja existe no arquivo;
-	char tipos[100][20];//vetor de strings que armazena os tipos de valores das colunas;
-	char nome_colunas[100][50];//vetor de strings que armazena os nomes de valores das colunas;
 	char string[300];//string que armazena cada linha do arquivo 1 por vez;
 	char *temp;//string que recebe o valor da chave primaria;
 	char opc = 's';//variavel que armazena a opcao de criar um nova linha ou nao;
-	tipo_var->chave = malloc(sizeof(char)*100);
-	tipo_var->valor_c = malloc(sizeof(char)*100);
 	char certeza;
 
 	bd = fopen("BD-ITP","r");
@@ -37,6 +29,7 @@ void apagar_t(){
 	if(ntabelas==0){
 		printf("Banco não tem tabelas!!\n");	
 	}
+	//enquanto o usuario quiser apagar tabelas ou enquanto existir tabelas no banco;
 	while (opc == 's'&&ntabelas>0){
 		//recebe o nome da tabela e abre o arquivo desejado;
 		printf("Selecione a tabela:\n");
@@ -60,7 +53,7 @@ void apagar_t(){
 			scanf("%c",&certeza);
 		}
 		fclose(bd);
-		//enquanto o usuario quiser apagar linhas ou enquanto o usuario nao digitar uma chave que existe no arquivo;
+		//se quiser mesmo apagar
 		if (certeza == 's'){
 			tem_tab=0;
 			bd = fopen("BD-ITP","r");//abre arquivo original para leitura;
@@ -72,14 +65,12 @@ void apagar_t(){
 				temp = malloc(sizeof(char)*50);
 				strcpy(temp," ");
 				strcpy(temp,tipo_var->nome_t);
-				strcat(temp,"\n\0");
-				//testa se a chave existe no arquivo;				 
+				strcat(temp,"\n\0");			 
 				if(strcmp(string,temp)!=0){
 					fputs(string,newbd);
 				}else{
 					tem_tab++;			
 				}
-				//printf("<%d>",strcmp(temp,tipo_var->chave));
 				free(temp);
 			}
 			fclose(bd);//fecha arquivo original;
@@ -88,14 +79,13 @@ void apagar_t(){
 			remove(tipo_var->nome_t);
 			rename("newfile","BD-ITP");//renomeia o novo arquivo com o nome do original;
 			}
-			//testa se a chave foi encontrada no arquivo original;
 			if(tem_tab==0||certeza=='n'){
-				//caso n tenha encontrado a chave no arquivo, pede para o usuario digitar outra chave;
+				//caso n tenha encontrado a tabela no arquivo, ou desistiu de apagar a tabela anterior;
 				printf("Deseja apagar outra tabela: s-sim n-nao\n");
 				scanf(" %c",&opc);
 			}else if(tem_tab == 1&&certeza=='s'){
 				printf("Tabela apagada!\n");
-				//caso tenha apagado a linha com sucesso, pergunta se o usuario deseja apagar outra linha;
+				//caso tenha apagado a tabela com sucesso, pergunta se o usuario deseja apagar outra tabela;
 				//rewind(arquivo);
 			
 				printf("Deseja apagar outra tabela: s-sim n-nao\n");
@@ -106,7 +96,5 @@ void apagar_t(){
 				
 		}
 	//desaloca todas as variaveis alocadas;
-	free(tipo_var->valor_c);
-	free(tipo_var->chave);
 	free(tipo_var);
 }
